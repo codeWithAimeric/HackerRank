6098c2385014c10f53cb7cace6475486c06c16e2
Aimeric <aimericjjb@gmail.com>
2023-07-16 14:35:52 +0300

    finish valid sudoku neetcode

--- neetcode/arrayAndHashing/validSudoku.js
+++ neetcode/arrayAndHashing/validSudoku.js
@@ -2,91 +2,50 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-var isValidSudoku = function(board) {
-    for(let i=0; i<board.length; i++){
-        for(let j=0; j<board[i].length; j++){
-            if(board[i][j] == "."){
-                board[i][j] = "";
-            }
+
+const isValidArray = (arr) => {
+    for(let i=0; i<arr.length; i++){
+        if(arr.indexOf(arr[i]) != arr.lastIndexOf(arr[i]) && arr[i] != "."){
+            console.log('arr[i', arr[i]);
+            console.log('arr = = =', arr);
+            return false; 
         }
     }
-    let tempSet = new Set();
+    return true; 
+}
+var isValidSudoku = function(board) {
     let tempTab = [];
-    let verticalTab = [];
-    for(let i=0; i<board.length; i++){
-        tempSet.add(board[i]);
-        tempTab = Array.from(tempSet);
-        if(tempTab.join('') == board[i].join('')){
+    let square = [];
+    for(let i=0; i<9; i++){
+        if(!isValidArray(board[i])){
+            return false; 
+        }
+    }
+    for(let i=0; i<9; i++){
+        for(let j=0; j<9; j++){
+            tempTab.push(board[j][i]);
+        }
+        if(!isValidArray(tempTab)){
             return false;
         }
-    }
-    tempSet = new Set();
-    tempTab = [];
-    for(let i=0; i<board.length; i++){
-        for(let j=0; j<board.length; j++){
-            tempTab.push(board[j][i]);
-            tempSet.add(board[j][i]);
-        }
-        verticalTab = Array.from(tempSet);
-        if(verticalTab.join('') == tempTab.join('')){
-            return false; 
-        }
-        tempSet = new Set();
         tempTab = [];
     }
-    let tempSet1 = new Set();
-    let tempTab1 = [];
-    let compareArray1 = [];
-    let tempSet2 = new Set();
-    let tempTab2 = [];
-    let compareArray2 = [];
-    let tempSet3 = new Set();
-    let tempTab3 = [];
-    let compareArray3 = [];
-    const subArray1 = [];
-    const subArray2 = [];
-    const subArray3 = [];
-    subArray1.push(board[0]);
-    subArray1.push(board[1]);
-    subArray1.push(board[2]);
-    console.log('subArray 1 ===', subArray1);
-    subArray2.push(board[3]);
-    subArray2.push(board[4]);
-    subArray2.push(board[5]);
-    console.log('subArray2 = = = = =', subArray2);
-    subArray3.push(board[6]);
-    subArray3.push(board[7]);
-    subArray3.push(board[8]);
-    console.log('subArray3 = = = = =', subArray3);
     for(let i=0; i<9; i+=3){
-        for(let j=0; j<=2; j++){
-            for(let k=i; k<=i+2; k++){
-                tempTab1.push(subArray1[j][k]);
-                tempSet1.add(subArray1[j][k]);
-                tempTab2.push(subArray2[j][k]);
-                tempSet2.add(subArray2[j][k]);
-                tempTab3.push(subArray3[j][k]);
-                tempSet3.add(subArray3[j][k]);
+        for(let j=0; j<9; j+=3){
+            square.push(board[i][j]);
+            square.push(board[i][j+1]);
+            square.push(board[i][j+2]);
+            square.push(board[i+1][j]);
+            square.push(board[i+1][j+1]);
+            square.push(board[i+1][j+2]);
+            square.push(board[i+2][j]);
+            square.push(board[i+2][j+1]);
+            square.push(board[i+2][j+2]);
+            if(!isValidArray(square)){
+                return false;
             }
+            square = [];
         }
-        compareArray1 = Array.from(tempSet1);
-        compareArray2 = Array.from(tempSet2);
-        compareArray3 = Array.from(tempSet3);
-        if(compareArray1.join('') == tempTab1.join('')){
-            return false; 
-        }
-        if(compareArray2.join('') == tempTab2.join('')){
-            return false; 
-        }
-        if(compareArray3.join('') == tempTab3.join('')){
-            return false; 
-        }
-        tempTab1 = [];
-        tempSet1 = new Set();
-        tempTab2 = [];
-        tempSet2 = new Set();
-        tempTab3 = [];
-        tempSet3 = new Set();
     }
     return true; 
 };
@@ -100,7 +59,7 @@ let  board =
 ,["7",".",".",".","2",".",".",".","6"]
 ,[".","6",".",".",".",".","2","8","."]
 ,[".",".",".","4","1","9",".",".","5"]
-,[".",".",".",".","8",".",".","7","9"]];
+,[".",".",".",".","8",".",".","7","9"]]; //true
 
 // let board = 
 // [["8","3",".",".","7",".",".",".","."]
